b04c3c4b4c7a947d504a9c4cce0f46b03279b900
Ivan Roth <i.roth@alumno.etec.um.edu.ar>
2025-09-23 13:02:19 -0300

    Etapa 1

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,8 +1,39 @@
 package org.example
 
-enum class Posicion {
-    ARQUERO,
-    DEFENSOR, 
-    MEDIOCAMPISTA,
-    DELANTERO
+import Posicion
+import Habilidades
+import Jugador
+
+fun main() {
+    
+    val messi = Jugador(
+        nombre = "Lionel Messi",
+        posicion = Posicion.DELANTERO,
+        habilidades = Habilidades(ataque = 95, defensa = 45, velocidad = 85)
+    )
+    
+    val neuer = Jugador(
+        nombre = "Manuel Neuer",
+        posicion = Posicion.ARQUERO,
+        habilidades = Habilidades(ataque = 10, defensa = 90, velocidad = 60)
+    )
+
+    val rasmussen = Jugador(
+        nombre = "Federico Rasmussen",
+        posicion = Posicion.DEFENSA,
+        habilidades = Habilidades(ataque = 38, defensa = 65, velocidad = 40)
+    )
+
+    // Mostrar información de los jugadores
+    println("Equipo de Fútbol Simulador")
+    println("------------------------------")
+    println("Jugador 1: ${messi.nombre} (${messi.posicion})")
+    println("  Ataque: ${messi.habilidades.ataque}")
+    println("  Defensa: ${messi.habilidades.defensa}")
+    println("  Velocidad: ${messi.habilidades.velocidad}")
+    
+    println("\nJugador 2: ${neuer.nombre} (${neuer.posicion})")
+    println("  Ataque: ${neuer.habilidades.ataque}")
+    println("  Defensa: ${neuer.habilidades.defensa}")
+    println("  Velocidad: ${neuer.habilidades.velocidad}")
 }
---
+++ app/src/main/kotlin/org/example/Habilidades.kt
@@ -0,0 +1,7 @@
+package org.example
+
+data class Habilidades(  
+    val ataque: Int,  
+    val defensa: Int,  
+    val velocidad: Int  
+)
